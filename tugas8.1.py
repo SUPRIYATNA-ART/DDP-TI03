d221c0bd45f7a62422c228a802819575a5366e42
SUPRIYATNA-ART <146078751+SUPRIYATNA-ART@users.noreply.github.com>
2023-11-22 17:50:42 +0700

    Add files via upload

---
+++ tugas8.1.py
@@ -0,0 +1,8 @@
+def profil(nama, alamat, gender, umur, hobi):
+    print("Nama:", nama)
+    print("Alamat:", alamat)
+    print("Gender:", gender)
+    print("Umur:", umur)
+    print("Hobi:", hobi)
+
+profil("supri", "parung bogor", "laki laki", 18, "olahraga")
